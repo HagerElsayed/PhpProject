85229a902156c674c7f1cd8030a890e8893f5415
sleeplessmind <eng.ahmed.abdraboh@gmail.com>
2017-02-10 14:10:12 +0200

    Product,path and subcategory

--- setup.php
+++ setup.php
@@ -21,7 +21,7 @@ if($mysqli->errno){
 }
 // create cart table
 $query = "create table if not exists cart(
-    id smallint unsigned auto_increment primary key, 
+    id smallint unsigned auto_increment primary key,
     quantity smallint unsigned,
     product_id smallint unsigned,
     foreign key (product_id) references product(id)
@@ -46,6 +46,47 @@ if(!$res){
     echo "posts table creation failed (".$mysqli->errno.") ".$mysqli->error;
     exit;
 }
+//create product table
+$query = "create table if not exists product(
+    id int unsigned not null auto_increment primary key,
+    subcategory_id int unsigned,
+    name varchar(100) not null,
+    description text,
+    price float not null,
+    path_id int unsigned not null,
+    foreign key (path_id) references path(id),
+    foreign key (subcategory_id) references subcategory(id)
+    )";
+$res = $mysqli->query($query);
+if(!$res){
+    echo "product table creation failed (".$mysqli->errno.") ".$mysqli->error;
+    exit;
+}//End of error check for product table creation
+
+//create subcategory table
+$query = "create table if not exists subcategory(
+    id int unsigned not null auto_increment primary key,
+    name varchar(100) not null,
+    category_name varchar(100) not null
+    )";
+$res = $mysqli->query($query);
+if(!$res){
+    echo "subcategory table creation failed (".$mysqli->errno.") ".$mysqli->error;
+    exit;
+}//End of error check for subcategory table creation
+
+//create path table
+$query = "create table if not exists path(
+    id int unsigned not null auto_increment primary key,
+    product_id unsigned not null,
+    path text not null,
+    foreign key (product_id) references product(id)
+    )";
+$res = $mysqli->query($query);
+if(!$res){
+    echo "path table creation failed (".$mysqli->errno.") ".$mysqli->error;
+    exit;
+}//End of error check for path table creation
 
 
 $mysqli->close();
