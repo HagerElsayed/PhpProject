084c339ddc67c80d4d7a19b365c03868f1fe07ba
HagerElsayed <hagerelsayed39@gmail.com>
2017-02-11 09:59:08 +0200

    add insert Function to product class

--- DatabaseClasses/product.php
+++ DatabaseClasses/product.php
@@ -13,19 +13,32 @@
  */
 class product {
 
+//     id int unsigned not null auto_increment primary key,
+//    subcategory_id int unsigned,
+//    name varchar(100) not null,
+//    quantity int,
+//    description text,
+//    photo text,
+//    price float not null,
+//    foreign key (subcategory_id) references subcategory(id)
+//    
+
+
     private $id;
     private $subcategory_id;
     private $name;
+    private $quantity;
     private $description;
     private $price;
-    private $path_id;
+    private $photo;
 
-    function __construct($id, $subcategory_id, $name, $description, $price, $path_id) {
+    function __construct($id, $subcategory_id, $name, $quantity, $description, $price, $photo) {
         $this->id = isset($this->id) ? $this->id : $id;
         $this->subcategory_id = isset($this->subcategory_id) ? $this->subcategory_id : $subcategory_id;
         $this->name = isset($this->name) ? $this->name : $name;
+        $this->quantity = isset($this->quantity) ? $this->quantity : $quantity;
         $this->description = isset($this->price) ? $this->price : $price;
-        $this->path_id = isset($this->path_id) ? $this->path_id : $path_id;
+        $this->photo = isset($this->photo) ? $this->photo : $photo;
     }
 
     function __get($name) {
@@ -35,5 +48,44 @@ class product {
     function __set($name, $value) {
         $this->$name = $value;
     }
+
+    //====Insert Function=====
+    function insert() {
+        $success = true;
+
+        //1_connect DB
+        $con = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+        if ($con->connect_errno) {
+            echo 'error connection to DB' . $con->connect_error . "<br>";
+            $success = false;
+            //exit;
+        }
+
+        //2-preparetion
+        $query = "insert into product(subcategory_id,name,quantity,description,photo)values(?,?,?,?,?)";
+        $stmt = $con->prepare($query);
+        if (!$stmt) {
+            echo 'error prpareint statment' . $con->error . "<br>";
+            $success = false;
+            //exit;
+        }
+        $result = $stmt->bind_param("isiss", $this->subcategory_id, $this->name, $this->quantity, $this->description, $this->photo);
+        if (!$result) {
+            echo 'binding failed' . $stmt->error;
+            $success = false;
+            //exit;
+        }
+
+        if (!$stmt->execute()) {
+            echo 'execuation failed' . $stmt->error;
+            $success = false;
+            //exit;
+        }
+        $stmt->close();
+        $con->close();
+        return $success;
+    }
+    
+    
 
 }
