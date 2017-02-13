892a1fd8d1dd42016cb30d64d0c61fbc0bc139dd
HindHesham <hindmohamed722@gmail.com>
2017-02-13 02:51:03 +0200

    home page

---
+++ User/home.php
@@ -0,0 +1,22 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="UTF-8">
+     <title>Home Page</title>
+     <link href="../bootstrap/Content/bootstrap.css" rel="stylesheet" type="text/css"/>
+   </head>
+   <body>
+     <form class="form-group" style="margin:50px">
+       <h2 > welcome</h2>
+      <div >
+       <a href="login.php"  class="btn btn-default btn-lg active" role="button">Login</a>
+       <a href="register.php" class="btn btn-default btn-lg active" role="button">Register</a>
+      </div>
+
+  </form>
+   </body>
+ </html>
