bd7ffcb6938680cb3cde50afac5eb5a23e8a62e1
HagerElsayed <hagerelsayed39@gmail.com>
2017-02-14 23:18:16 +0200

    Add Links to AdminTest

--- Admin/adminTest.php
+++ Admin/adminTest.php
@@ -1,1 +1,20 @@
+<?php
+//require_once '../config.php';
+require_once '../DatabaseClasses/user.php';
+
+$user = isLogged();
+if(isAdmin($user))
+{
+?>
+<a href="../Admin/mangeUsers.php">Manage Users</a>
+<a href="">Manage Product</a>
+
+
+<?php
+
+}
+?>
+
+
+<a href="../User/logout.php">Logout</a>
 
